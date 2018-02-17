b5e7503b76cd630a6aea09d07796350667fb7b67
piyumisudusinghe <piyumi.15@cse.mrt.ac.lk>
2018-02-17 16:48:04 +0530

    Mobile application- App with firebase connection

--- app/src/main/java/com/example/piumi/disaster_management/LoginActivity.java
+++ app/src/main/java/com/example/piumi/disaster_management/LoginActivity.java
@@ -1,23 +1,50 @@
 package com.example.piumi.disaster_management;
 
+
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class LoginActivity extends AppCompatActivity implements  View.OnClickListener{
 
     private Button btnCreateAccount;
     private Button btnForgetPwd;
+    private Button btnLogin;
+    private FirebaseAuth firebaseAuth;
+    private static String TAG;
+    private EditText txtEmail;
+    private EditText txtPwd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main6);
+        firebaseAuth =  FirebaseAuth.getInstance();
         btnCreateAccount =  findViewById(R.id.btn_createAccount);
         btnForgetPwd=findViewById(R.id.btn_forgotPwd);
+        btnLogin = findViewById(R.id.btn_login);
         btnCreateAccount.setOnClickListener(this);
         btnForgetPwd.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
+        TAG = LoginActivity.class.getName();
+        txtEmail = (EditText)findViewById(R.id.txt_email);
+        txtPwd = (EditText)findViewById(R.id.txt_pwd);
+
+
 
     }
 
@@ -35,9 +62,55 @@ public class LoginActivity extends AppCompatActivity implements  View.OnClickLis
                 Intent intent2 = new Intent(LoginActivity.this,ForgotPasswordActivity.class);
                 startActivity(intent2);
                 break;
+            case R.id.btn_login:
+                signUp();
+
 
 
         }
 
     }
+
+    public void signUp(){
+        String email = txtEmail.getText().toString().trim();
+        String password = txtPwd.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Please enter email",Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Please enter password",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            Toast.makeText(LoginActivity.this, "Login is successfull.",
+                                    Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                           // updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
+
+    }
+
+
+
+
 }
--- app/src/main/java/com/example/piumi/disaster_management/RegisterActivity.java
+++ app/src/main/java/com/example/piumi/disaster_management/RegisterActivity.java
@@ -1,13 +1,172 @@
 package com.example.piumi.disaster_management;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    private EditText txtEmail;
+    private EditText txtPwd;
+    private FirebaseAuth firebaseAuth;
+    private Button btnRegister;
+    private EditText firstName;
+    private EditText lastName;
+    private EditText confirmPwd;
+    private static  String TAG;
+    private Button btn_cancel;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("mobile_user");
+
+        TAG = RegisterActivity.class.getName();
+
+        txtEmail = (EditText)findViewById(R.id.reg_email);
+        txtPwd = (EditText)findViewById(R.id.reg_password);
+        firstName = (EditText)findViewById(R.id.firstname);
+        lastName = (EditText)findViewById(R.id.lastname);
+        confirmPwd = (EditText)findViewById(R.id.confirm_password);
+
+        btnRegister = findViewById(R.id.register_btn);
+        btnRegister.setOnClickListener(this);
+
+        btn_cancel = findViewById(R.id.cancel_btn);
+        btn_cancel.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+
+            case R.id.register_btn:
+                registerUser();
+                break;
+            case R.id.cancel_btn:
+                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+        }
+
+    }
+
+    @Override
+    public  void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    public void registerUser(){
+        String email = txtEmail.getText().toString().trim();
+        String password = txtPwd.getText().toString().trim();
+        String confirm_password = confirmPwd.getText().toString().trim();
+        final String firstname = firstName.getText().toString().trim();
+        final String lastname = lastName.getText().toString().trim();
+
+        if(TextUtils.isEmpty(firstname)){
+            Toast.makeText(this,"Please enter first name",Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+        if(TextUtils.isEmpty(lastname)){
+            Toast.makeText(this,"Please enter last name",Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Please enter email",Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+
+        if(TextUtils.isEmpty(confirm_password)){
+            Toast.makeText(this,"Please enter password again",Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Please enter password",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            verifyEmail();
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            //updateUI(user);
+                            String user_id = user.getUid();
+                            databaseReference = database.getReference("mobile_user/"+user_id);
+                            HashMap<String, String> map = new HashMap<String, String>();
+                            map.put("first_name",firstname);
+                            map.put("last_name",lastname);
+                            databaseReference.setValue(map);
+
+                        } else {
+
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed."+task.getException(),
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    public void verifyEmail(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user!= null){
+            user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(RegisterActivity.this, "Check your Email for verification",Toast.LENGTH_SHORT).show();
+                        FirebaseAuth.getInstance().signOut();
+                    }
+                }
+            });
+        }
     }
 }
